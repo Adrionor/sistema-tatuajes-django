8a1de063fc7bf10adf28db4ef690c996d80f3538
Adrionor <jorgeadrian.dp@gmail.com>
2026-03-22 14:41:42 -0700

    chore: eliminar archivos Django duplicados de la raíz del repo

--- cotizaciones/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Cotizacion
-
-admin.site.register(Cotizacion)
--- portafolio/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Trabajo
-
-def galeria_portafolio(request):
-    # Obtenemos todos los trabajos guardados en la base de datos
-    trabajos = Trabajo.objects.all()
-    
-    # Se los enviamos a un archivo HTML llamado 'galeria.html'
-    return render(request, 'portafolio/galeria.html', {'trabajos': trabajos})
