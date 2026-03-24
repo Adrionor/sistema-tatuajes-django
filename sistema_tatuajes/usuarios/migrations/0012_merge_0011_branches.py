6e22b200040415ace6d3d667e5dd7620854b4c92
Adrionor <jorgeadrian.dp@gmail.com>
2026-03-24 11:45:55 -0700

    fix(migrations): merge rama 0011 (logo_hero + datos_bancarios)

---
+++ sistema_tatuajes/usuarios/migrations/0012_merge_0011_branches.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('usuarios', '0011_alter_configuracionestudio_logo_hero'),
+        ('usuarios', '0011_perfil_datos_bancarios'),
+    ]
+
+    operations = [
+    ]
